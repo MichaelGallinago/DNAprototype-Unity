85b50bc280ad74040b039169dd2a3cc18f59b026
MichaelGallinago <michael.gallinago@gmail.com>
2024-12-09 00:21:52 +0500

    Character preparations

---
+++ Assets/Character/Character.cs
@@ -0,0 +1,16 @@
+using System;
+using IK2D.Bodies;
+using UnityEngine;
+
+namespace Character
+{
+    public class Character : MonoBehaviour
+    {
+        [SerializeField] private AnthroBody _body;
+
+        private void FixedUpdate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
