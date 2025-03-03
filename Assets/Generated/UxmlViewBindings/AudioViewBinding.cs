c0978ffcb46d90d8db4bc39db61ae9248354f746
MichaelGallinago <michael.gallinago@gmail.com>
2025-03-04 00:02:29 +0500

    AudioViewBinding fix

--- Assets/Generated/UxmlViewBindings/AudioViewBinding.cs
+++ Assets/Generated/UxmlViewBindings/AudioViewBinding.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine.UIElements;
 using System;
 // ReSharper disable CheckNamespace
@@ -11,12 +11,16 @@ namespace UxmlViewBindings
     public readonly struct AudioViewBinding
     {
         [NotNull] public readonly VisualElement Root;
-        [NotNull] public readonly ApplyViewBinding Apply;
+        [NotNull] public readonly CustomSliderViewBinding Sound;
+        [NotNull] public readonly CustomSliderViewBinding Music;
+        [NotNull] public readonly BackButtonViewBinding Apply;
 
         public AudioViewBinding(VisualElement root)
         {
             Root = root;
-            Apply = new ApplyViewBinding(root);
+            Sound = new CustomSliderViewBinding(root.Q<VisualElement>("Sound") ?? throw new NullReferenceException("\"Sound\" not found!"));
+            Music = new CustomSliderViewBinding(root.Q<VisualElement>("Music") ?? throw new NullReferenceException("\"Music\" not found!"));
+            Apply = new BackButtonViewBinding(root.Q<VisualElement>("Apply") ?? throw new NullReferenceException("\"Apply\" not found!"));
         }
     }
 }
